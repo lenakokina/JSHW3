cbdea18f7e827ff7c930051b0ab1f9aad53a3dad
Elena Kokina <3400051@gmail.com>
2021-05-07 12:25:00 +0300

    bug fix

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-
 function getOperator(message) {
-        let operator;
-        let error;
-        do {
-          operator = prompt(message);
-          error = (operator !== '+' && operator !== '-' && operator !== '*' && operator !== '/');
-          if (error) {
-            alert( ` the entered value ${operator} is not an arithmetic character `);
-          }
-  }
-  while (error);
-  return operator;
+  let operator;
+  do {
+    operator = prompt(message);
+    }  
+    while (operator !== '+' && operator !== '-' && operator !== '*' && operator !== '/');
+    return operator;
 }
+
+// function getOperator(message) {
+//   let operator;
+//    let error;
+//      do {
+//       operator = prompt(message);
+//        error = (operator !== '+' && operator !== '-' && operator !== '*' && operator !== '/');
+//      if (error) {
+//        alert( ` the entered value ${operator} is not an arithmetic character `);
+//        }
+//   }
+//   while (error);
+//   return operator;
+// }
 
 function getNumber(message) {
-  let result = prompt(message);
-  while (isNaN(result)) {
-    alert(`${result}this is not a number`);
-    result = prompt(message);
+  let result;
+  do {
+  result = +prompt(message);
   }
+  while (isNaN(result)) 
   return result;
 }
+// function getNumber(message) {
+//   let result = prompt(message);
+//   while (isNaN(result)) {
+//     alert(`${result} this is not a number`);
+//     result = prompt(message);
+//   }
+//   return result;
+// }
 
 let validOperator = getOperator('enter arithmetic action (options: "+", "-", "*", "/")');
-let firstNumber = +getNumber('enter the first nbmber');
+let firstNumber = +getNumber('enter the first number');
 let secondNumber = +getNumber('enter the second number');
 let calculation;
 
